79d7c622c5ed01bfb00e2e1c21461f21a198889f
DSBLD <amo48520@gmail.com>
2021-12-01 21:14:03 +0300

    V.1.0

---
+++ README.txt
@@ -0,0 +1,7 @@
+Привет!
+Это мой тестовый проект посвященный знакомству с основными командами Framework Cypress.
+В качестве базы написан автотест на некоторые элементы стартовой страницы Google (Google.com)
+
+Hello!
+This is my test project for getting started with running the Framework Cypress command.
+As a base, an autotest was written on some elements of the google start page (Google.com)
--- cypress/integration/FirstTest.spec.js
+++ cypress/integration/FirstTest.spec.js
@@ -1,42 +1,48 @@
 describe('Тестирование некоторых элементов стартовой страницы Goggle', () => {
-    it('Посещение Google и корректное отображение заголовка', () => {
+    beforeEach(() => {
       cy.visit('https://google.com')
+    })
+  
+    it('Посещение Google и корректное отображение заголовка', () => {
       cy.title().should('contain', 'Google');    
     });
   
-    it("Поиск по слову Yandex", () => {
+    it("Поиск по слову Yandex и клик на первый результат", () => {
       cy.get('input[title="Поиск"]').type('Yandex{enter}')
-      cy.title().should('contain', 'Yandex');
-    });
-
-    it('Клик на первый результат', () => {
+      cy.title().should('contain', 'Yandex');    
       cy.get('#search a')
         .invoke('attr', 'href')
         .then(href => console.log(href));
     });
 
-    it('Переход назад. Нажатие клавиши "Мне повезёт!"', () => {
-      cy.go(-1)      
+    it('Нажатие клавиши "Мне повезёт!"', () => {            
       cy.get('.FPdoLc > center > .RNmpXc').click()
-      cy.title().should('contain', 'Дудлы Google');
+      cy.url().should('include', 'doodles');
     });
 
-    it('Переход назад. Нажатие на логотип "Google"', () => {
-      cy.go(-1)      
+    it('Нажатие на логотип "Google"', () => {            
       cy.get('#hplogo').click()      
     });
 
-    it('Переход назад. Нажатие на " Всё о Google"', () => {
-      cy.go(-1)      
+    it('Нажатие на " Всё о Google"', () => {            
       cy.get('[href="https://about.google/?utm_source=google-RU&utm_medium=referral&utm_campaign=hp-footer&fg=1"]').click()
       cy.title().should('contain', 'Google - Компания');
     });
 
-    it('Переход назад. Нажатие на значок "Поделиться"', () => {
-      cy.go(-1)      
+    /* it('Нажатие на значок "Поделиться"', () => {            
       cy.get('.GVqCqe').click()
       cy.url().should('include', 'ddle=1');
-      cy.get('.ddlshare-dialog-title-close').click()
+      cy.get('.ddlshare-dialog-title-close').click() 
+    }); */
+
+    it ('Нажатие на текст "Реклама"', () => {
+      cy.get('[href="https://www.google.com/intl/ru_ru/ads/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1"]').click()
+      cy.url().should('include', 'https://ads.google')
+    });
+    
+    it ('Нажатие на текст "Для бизнеса"', () => {
+      cy.get('[href="https://www.google.com/services/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpbfooter&fg=1"]').click()
+      cy.url().should('include', '/services')
     });
     
   })
