9a9a7f620ce2b916b29be1e2439297ef9209b684
DSBLD <amo48520@gmail.com>
2021-12-08 21:48:42 +0300

    V.1.1

--- README.txt
+++ README.txt
@@ -2,6 +2,14 @@
 Это мой тестовый проект посвященный знакомству с основными командами Framework Cypress.
 В качестве базы написан автотест на некоторые элементы стартовой страницы Google (Google.com)
 
+V.1.1 (08.12.21)
+- Содержит автотест гиперссылок: "Конфидециальность", "Условия", "Как работает Google Поиска"
+- Недоработанный автотест выпадающего меню "Настройки"
+
 Hello!
 This is my test project for getting started with running the Framework Cypress command.
 As a base, an autotest was written on some elements of the google start page (Google.com)
+
+V.1.1(08.12.21)
+- Contains autotest of hyperlinks: "Privacy", "Terms", "How Google Search works"
+- Incomplete autotest of the drop-down menu "Settings"
--- cypress/integration/FirstTest.spec.js
+++ cypress/integration/FirstTest.spec.js
@@ -24,7 +24,7 @@ describe('Тестирование некоторых элементов ста�
       cy.get('#hplogo').click()      
     });
 
-    it('Нажатие на " Всё о Google"', () => {            
+    it('Нажатие на гиперссылку "Всё о Google"', () => {            
       cy.get('[href="https://about.google/?utm_source=google-RU&utm_medium=referral&utm_campaign=hp-footer&fg=1"]').click()
       cy.title().should('contain', 'Google - Компания');
     });
@@ -35,14 +35,49 @@ describe('Тестирование некоторых элементов ста�
       cy.get('.ddlshare-dialog-title-close').click() 
     }); */
 
-    it ('Нажатие на текст "Реклама"', () => {
+    it ('Нажатие на гиперссылку "Реклама"', () => {
       cy.get('[href="https://www.google.com/intl/ru_ru/ads/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1"]').click()
       cy.url().should('include', 'https://ads.google')
     });
     
-    it ('Нажатие на текст "Для бизнеса"', () => {
+    it ('Нажатие на гиперссылку "Для бизнеса"', () => {
       cy.get('[href="https://www.google.com/services/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpbfooter&fg=1"]').click()
       cy.url().should('include', '/services')
     });
     
+    it ('Нажатие на гиперссылку "Как работает Google Поиск"', () => {
+      cy.get('body > div.L3eUgb > div.o3j99.c93Gbe > div.KxwPGc.SSwjIe > div.KxwPGc.AghGtd > a:nth-child(4)').click()
+      cy.url().should('include', '/search')
+    });
+
+    it ('Нажатие на гиперссылку "Конфидециальность"', () => {
+      cy.get('body > div.L3eUgb > div.o3j99.c93Gbe > div.KxwPGc.SSwjIe > div.KxwPGc.iTjxkf > a:nth-child(1)').click()
+      cy.url().should('include', '/privacy')
+    });
+
+    it ('Нажатие на гиперссылку "Условия"', () => {
+      cy.get('body > div.L3eUgb > div.o3j99.c93Gbe > div.KxwPGc.SSwjIe > div.KxwPGc.iTjxkf > a:nth-child(2)').click()
+      cy.url().should('include', '/terms')
+    });
+  
+  describe('Тестирование выпадающего меню "Настройки"',()=>{
+    beforeEach(() => {
+      cy.visit('https://google.com')
+      it('Нажатие на "Настройки"', ()=>{
+        cy.get('#Mses6b').click()       
+      })
+
+    it('Нажатие на "Настройки поиска"', ()=>{  
+      cy.contains('class = "EzVRq"','Настройки поиска')
+        .click()
+      cy.url().should('include', '/preferences')
+    })
+
+    it('Нажатие на "Расширенный поиск"', ()=>{  
+      cy.get('#dEjpnf > li:nth-child(2) > a').click()
+      cy.url().should('include', '/advanced_search')
+    })
   })
+})
+
+})
